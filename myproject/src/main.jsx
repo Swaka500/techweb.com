fcaa87e6e619288163cee68c609395ee9aaf469a
Derrick Swaka <derrickswaka910@gmail.com>
2026-04-30 19:29:12 +0300

    Initial React website

--- myproject/src/main.jsx
+++ myproject/src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/org.com">
+    <BrowserRouter basename="/webtech.com">
       <App />
     </BrowserRouter>
   </StrictMode>
--- myproject/vite.config.js
+++ myproject/vite.config.js
@@ -3,5 +3,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "/org.com/",
+  base: "/webtech.com/",
 })
